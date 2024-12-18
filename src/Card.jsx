5216adf7699a90ec2e2d626020bcec8efa07bcab
Jacob Adams big-boi <jacob.adams121202@gmail.com>
2024-12-17 18:09:12 -0800

    card implemented

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,14 +1,44 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import "./Card.css";
 
+function Card(props) {
+  const [wasClicked, setWasClicked] = useState(false);
 
-function Card(){
-  return(
-    <div className='main'>
-      <div className='image'></div>
-      <h3 className='name'></h3>
+  const handleClick = () => {
+    setWasClicked(!wasClicked);
+    if (wasClicked) {
+      props.endCurrentStage();
+    } else {
+      props.incrementScore;
+    }
+    props.randomizeCards();
+  };
+
+  useEffect(() => {
+    if (props.reset) {
+      setWasClicked(false);
+    }
+  }, [props.reset]);
+
+  return (
+    <div className="card" key={props.image} onClick={handleClick}>
+      <img src={props.image} alt={props.name}></img>
+      <br></br>
+      <span>
+        <strong>{props.name}</strong>
+      </span>
     </div>
-
-  )
+  );
 }
+
+Card.propTypes = {
+  image: PropTypes.string,
+  name: PropTypes.string,
+  reset: PropTypes.bool,
+  endCurrentStage: PropTypes.func,
+  incrementScore: PropTypes.func,
+  randomizeCards: PropTypes.func,
+};
 
 export default Card;
